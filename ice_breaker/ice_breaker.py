c6209573b482b6497dad37f48f883293313fc108
JunhyeokBaek <junhyeokbaek0306@gmail.com>
2025-05-12 23:22:30 +0900

    Update

--- ice_breaker/ice_breaker.py
+++ ice_breaker/ice_breaker.py
@@ -1,35 +1,30 @@
 from dotenv import load_dotenv
-from langchain_core.prompts import PromptTemplate
+from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
-from langchain_ollama import ChatOllama
-from langchain_core.output_parsers import StrOutputParser
 from langchain_google_genai import ChatGoogleGenerativeAI
+from third_parties.linkedin import scrape_linkedin_profile
 
-information = """
-    Elon Reeve Musk (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a businessman known for his leadership of Tesla, SpaceX, and X (formerly Twitter). Since 2025, he has been a senior advisor to United States President Donald Trump and the de facto head of the Department of Government Efficiency (DOGE). Musk has been the wealthiest person in the world since 2021; as of March 2025, Forbes estimates his net worth to be US$345 billion. He was named Time magazine's Person of the Year in 2021.
+if __name__ == "__main__":
+    load_dotenv()
 
-Born to a wealthy family in Pretoria, South Africa, Musk emigrated in 1989 to Canada. He graduated from the University of Pennsylvania in the U.S. before moving to California to pursue business ventures. In 1995, Musk co-founded the software company Zip2. Following its sale in 1999, he co-founded X.com, an online payment company that later merged to form PayPal, which was acquired by eBay in 2002. That year, Musk also became a U.S. citizen.
+    print("Hello LangChain")
 
-In 2002, Musk founded the space technology company SpaceX, becoming its CEO and chief engineer; the company has since led innovations in reusable rockets and commercial spaceflight. Musk joined the automaker Tesla as an early investor in 2004 and became its CEO and product architect in 2008; it has since become a leader in electric vehicles. In 2015, he co-founded OpenAI to advance artificial intelligence research but later left; growing discontent with the organization's direction in the 2020s led him to establish xAI. In 2022, he acquired the social network Twitter, implementing significant changes and rebranding it as X in 2023. In January 2025, he was appointed head of Trump's newly created DOGE. His other businesses include the neurotechnology company Neuralink, which he co-founded in 2016, and the tunneling company the Boring Company, which he founded in 2017.
-"""
-
-if __name__ == '__main__':
-    print("Hello langchain")
-    
     summary_template = """
-        given the information {information} about a person from I want to create:
-        1. a short summary
-        2. two interestion facts about them
+    given the Linkedin information {information} about a person I want you to create:
+    1. A short summary
+    2. two interesting facts about them
     """
-    
-    summary_promt_template = PromptTemplate(input_variables=["information"], template=summary_template)
-    
-    # llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-    # llm = ChatOllama(model="deepseek-r1:8b")
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    # llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
     llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0)
-    
-    chain = summary_promt_template | llm | StrOutputParser()
-    
-    res = chain.invoke(input={"information": information})
-    
+    chain = summary_prompt_template | llm
+    linkedin_data = scrape_linkedin_profile(
+        linkedin_profile_url="https://www.linkedin.com/in/eden-marco/"
+    )
+    res = chain.invoke(input={"information": linkedin_data})
+
     print(res)
